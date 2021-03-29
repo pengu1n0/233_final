ec32a40f2b7d245729087134151ec50ca6f77eef
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-29 19:03:06 +0700

    นึกวิธีทำไม่ออก

--- nearest.c
+++ nearest.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int n[4],i;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	int k[10],n[10],i,j;
+	scanf("%d %d",&k[10],&n[10]);
+	if(k>n)
 	{
-		
+		int sum1=k[0];
+		int sum2=n[0];
+		i==sum1;
+		j==sum2;
+		printf("%d%d",i,j);
 	}
 	
+	return 0;
 }
