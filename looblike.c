3371c093bb449d2bb7d4097ee9dd3fb5a84e0a3e
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-29 17:47:37 +0700

    เต็ม

--- looblike.c
+++ looblike.c
@@ -3,24 +3,24 @@ int main()
 {
 	int n,i,m;
 	scanf("%d",&n);
-	int A[10000]={0},max=0;
+	int A[1000]={0},max=0;
 	for(i=1;i<=n;i++)
 	{
 		scanf("%d",&m);
 		A[m]+=1;
 	}
-	for(i=0;i<10000;i++)
+	for(i=0;i<1000;i++)
 	{
 		if(max<A[i])
 		{
 			max=A[i];
 		}
 	}
-	for(i=0;i<=10000;i++)
+	for(i=0;i<=1000;i++)
 	{
 		if(max==A[i])
 		{
-			printf("%d",i);
+			printf("%d ",i);
 		}
 	}
 	
