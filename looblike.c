451c64cddeb9efd2914fa4932860454912d984b9
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-29 17:27:44 +0700

    ยังไม่เส็จ

--- looblike.c
+++ looblike.c
@@ -3,8 +3,16 @@ int main()
 {
 	int n,i,m;
 	scanf("%d",&n);
+	int A[10000]={0},max=0;
 	for(i=1;i<=n;i++)
 	{
 		scanf("%d",&m);
+		A[m]+=1;
 	}
+	for(i=0;i<10000;i++)
+	{
+		
+	}
+	
+	return 0;
 }
